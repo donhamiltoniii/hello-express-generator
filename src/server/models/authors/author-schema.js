1ea5435b4985612c2702aa226915e127184379c2
Don Hamilton III <donhamiltoniii@gmail.com>
2019-06-20 11:08:43 -0400

    Minor typo fixes

--- src/server/models/authors/author-schema.js
+++ src/server/models/authors/author-schema.js
@@ -1,9 +1,8 @@
-const mongoose = require('mongoose')
-
-const bookSchema = require('../books/book-schema')
+const mongoose = require("mongoose");
+const bookSchema = require("../books/book-schema");
 
 module.exports = new mongoose.Schema({
   firstName: { type: String, required: true },
   lastName: { type: String, required: true },
   books: [bookSchema]
-})
+});
